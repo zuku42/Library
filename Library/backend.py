56a13c584da1a6dab97908fce5ef7a4539fc791f
Banksy <zukiewicz.piotr@gmail.com>
2020-10-09 13:31:51 +0200

    Encapsulate connection establishing code in a decorator
    Code that establishes connection with the database and
    creates a table was split in two parts. The connection
    establishments now occurs in a decorator (connect). The "create_table" funcion, as well as other functions requiring connection with the database that will be developed in the future, will be decorated by that decorator

--- Library/backend.py
+++ Library/backend.py
@@ -1,7 +1,16 @@
 import sqlite3
 
-conn = sqlite3.connect("library_database.db")
-cur = conn.cursor()
-cur.execute("CREATE TABLE if not EXISTS books (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, year_read INTEGER)")
-conn.commit()
-conn.close()
+def connect(func):
+	def wrapper_connect():
+		conn = sqlite3.connect("library_database.db")
+		cur = conn.cursor()
+		func(cur,conn)
+		conn.close()
+	return wrapper_connect
+
+@connect
+def create_table(cursor, connection):
+	cursor.execute("CREATE TABLE if not EXISTS books (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, year_read INTEGER)")
+	connection.commit()
+
+create_table()
