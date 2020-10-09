43cbf1dcc2f411630dadaf0a6dc6efb2a3ab4add
Banksy <zukiewicz.piotr@gmail.com>
2020-10-09 13:45:46 +0200

    Improve the decorator, add new function
    Improved the connect decroator so that now it is able to effectively decorate functions that return some values.
    Added a view() function that fetches all records of the books table.

--- Library/backend.py
+++ Library/backend.py
@@ -4,13 +4,20 @@ def connect(func):
 	def wrapper_connect():
 		conn = sqlite3.connect("library_database.db")
 		cur = conn.cursor()
-		func(cur,conn)
+		returned_value = func(cur, conn)
 		conn.close()
+		return returned_value
 	return wrapper_connect
 
 @connect
 def create_table(cursor, connection):
 	cursor.execute("CREATE TABLE if not EXISTS books (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, year_read INTEGER)")
 	connection.commit()
+
+@connect
+def view(cursor, connection):
+	cursor.execute("SELECT * FROM books")
+	rows = cursor.fetchall()
+	return rows
 
 create_table()
