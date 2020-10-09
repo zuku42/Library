a07653017771cdae28df43097a3ebd1464e0517e
Banksy <zukiewicz.piotr@gmail.com>
2020-10-09 17:32:00 +0200

    Create a python file for SQL commands
    Strings constants with commands in SQL were moved to a seperate file to increase readibility of the main backend script.

---
+++ Library/sql_commands.py
@@ -0,0 +1,8 @@
+CREATE_TEXT = "CREATE TABLE if not EXISTS books (id INTEGER PRIMARY KEY,\
+				title TEXT, author TEXT, year INTEGER, year_read INTEGER)"
+VIEW_TEXT = "SELECT * FROM books"
+SEARCH_TEXT = "SELECT * FROM books WHERE title=? OR author=? OR year=? OR year_read=?"
+INSERT_TEXT = "INSERT INTO books VALUES (NULL,?,?,?,?)"
+UPDATE_TEXT = "UPDATE books SET title=?, author=?, year=?, year_read=? WHERE id=?"
+DELETE_TEXT = "DELETE FROM books WHERE id=?"
+CLEAR_TEXT = "DELETE FROM books"
