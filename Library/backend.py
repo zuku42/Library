5f21eec4950f2093d4e02352193c1b514b8323f7
Banksy <zukiewicz.piotr@gmail.com>
2020-10-09 17:14:13 +0200

    Change the architecture of the script to OOP
    Edited the code so that it does not use decorated functions anymore, but relies on class methods instead.

--- Library/backend.py
+++ Library/backend.py
@@ -1,78 +1,58 @@
 import sqlite3
 
-def connect(func):
-	"""
-	This is a decorator that connects to the library
-	database, performs operations on the records of
-	the database according to the instructions of the
-	function it is decorating and closes the connection
-	with the database.
-	"""
-	def wrapper_connect(*args, **kwargs):
-		conn = sqlite3.connect("library_database.db")
-		cur = conn.cursor()
-		returned_value = func(cur, conn, *args, **kwargs)
-		conn.close()
-		return returned_value
-	return wrapper_connect
 
-@connect
-def create_table(cursor, connection):
-	"""
-	Creates the table in the database if it does not
-	already exist.
-	"""
-	cursor.execute("CREATE TABLE if not EXISTS books (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, year_read INTEGER)")
-	connection.commit()
+class DatabaseConnection():
 
-@connect
-def view(cursor, connection):
-	"""Returns all records of the database"""
-	cursor.execute("SELECT * FROM books")
-	rows = cursor.fetchall()
-	return rows
+	def __init__(self, db_name):
+		"""Establishes connection with the database"""
+		self.conn = sqlite3.connect(db_name)
+		self.cur = self.conn.cursor()
+		self.create_table()
+	
+	def create_table(self):
+		"""Creates a table in the database"""
+		self.cur.execute("CREATE TABLE if not EXISTS books (id INTEGER PRIMARY KEY,\
+						  title TEXT, author TEXT, year INTEGER, year_read INTEGER)")
+		self.conn.commit()
 
-@connect
-def search(cursor, connection, title="", author="", year="", year_read=""):
-	"""
-	Returns all records of the database that meet 
-	the search criteria.
-	"""
-	cursor.execute("SELECT * FROM books WHERE title=? OR author=? OR year=? OR year_read=?",(title, author, year, year_read))
-	rows = cursor.fetchall()
-	return rows
+	def view(self):
+		"""Returns all records of the database"""
+		self.cur.execute("SELECT * FROM books")
+		rows = self.cur.fetchall()
+		return rows
 
-@connect
-def insert(cursor, connection, title, author, year, year_read):
-	"""
-	Inserts new record into the database
-	"""
-	cursor.execute("INSERT INTO books VALUES (NULL,?,?,?,?)",(title, author, year, year_read))
-	connection.commit()
+	def search(self, title="", author="", year="", year_read=""):
+		 """Returns records of the database that 
+		 	meet the search criteria"""
+		self.cur.execute("SELECT * FROM books WHERE title=? OR author=? OR year=?\
+						  OR year_read=?",(title, author, year, year_read))
+		rows = self.cur.fetchall()
+		return rows
 
-@connect
-def update(cursor, connection, id, title, author, year, year_read):
-	"""
-	Updates one of the records of the database based
-	on its id
-	"""
-	cursor.execute("UPDATE books SET title=?, author=?, year=?, year_read=? WHERE id=?",(title, author, year, year_read, id))
-	connection.commit()
+	def insert(self, title, author, year, year_read):
+		"""Inserts a new record into the database"""
+		self.cur.execute("INSERT INTO books VALUES (NULL,?,?,?,?)",\
+					    (title, author, year, year_read))
+		self.conn.commit()
 
-@connect
-def delete(cursor, connection, id):
-	"""
-	Deletes one of the records of the database based
-	on its id
-	"""
-	cursor.execute("DELETE FROM books WHERE id=?",(id,))
-	connection.commit()
+	def update(self, id, title, author, year, year_read):
+		"""Updates one of the records of the database 
+			based on its id"""
+		self.cur.execute("UPDATE books SET title=?, author=?, year=?, year_read=?\
+						  WHERE id=?",(title, author, year, year_read, id))
+		self.conn.commit()
 
-@connect
-def clear(cursor, connection):
-	"""
-	Clears the database
-	"""
-	cursor.execute("DELETE FROM books")
-	connection.commit()
+	def delete(self, id):
+		"""Deletes one of the records of the database
+			based on its id"""
+		self.cur.execute("DELETE FROM books WHERE id=?",(id,))
+		self.conn.commit()
 
+	def clear(self):
+		"""Deletes all records from the database"""
+		self.cur.execute("DELETE FROM books")
+		self.conn.commit()
+
+	def close(self):
+		"""Closes connection with the database"""
+		self.conn.close()
