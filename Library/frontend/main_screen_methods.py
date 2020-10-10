2d2b917a02b52292af955a78499ea4a6e4974d29
Banksy <zukiewicz.piotr@gmail.com>
2020-10-10 10:23:25 +0200

    Add remaining methods
    Renamed the file from frontend_commands.py to main_screen_methods.py
    to indicate this module is an extenstion of the MainScreen class
    inside the main_screen.py module.
    Added remaining methods including search, insert, delete, update,
    and clear.

---
+++ Library/frontend/main_screen_methods.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+
+
+def get_selected_row(self, event):
+	global selected_id
+	try:
+		index = self.list1.curselection()[0]
+		selected_id = self.list1.get(index)[0]
+		self.e1.delete(0, tk.END)
+		self.e1.insert(tk.END, self.list1.get(index)[1])
+		self.e2.delete(0, tk.END)
+		self.e2.insert(tk.END, self.list1.get(index)[2])
+		self.e3.delete(0, tk.END)
+		self.e3.insert(tk.END, self.list1.get(index)[3])
+		self.e4.delete(0, tk.END)
+		self.e4.insert(tk.END, self.list1.get(index)[4])
+	except IndexError:
+		pass
+
+def view_command(self):
+	self.list1.delete(0, tk.END)
+	for row in self.db_conn.view():
+		self.list1.insert(tk.END, row)
+
+def search_command(self):
+	self.list1.delete(0, tk.END)
+	for row in self.db_conn.search(self.title_text.get(), self.author_text.get(), self.year_text.get(), self.year_read_text.get()):
+		self.list1.insert(tk.END, row)
+
+def insert_command(self):
+	self.db_conn.insert(self.title_text.get(), self.author_text.get(), self.year_text.get(), self.year_read_text.get())
+	self.list1.insert(tk.END, self.db_conn.view()[-1])
+
+def delete_command(self):
+	self.list1.delete(0, tk.END)
+	self.db_conn.delete(selected_id)
+	for row in self.db_conn.view():
+		self.list1.insert(tk.END, row)
+
+def update_command(self):
+	self.list1.delete(0, tk.END)
+	self.db_conn.update(selected_id, self.title_text.get(), self.author_text.get(), self.year_text.get(), self.year_read_text.get())
+	for row in self.db_conn.view():
+		self.list1.insert(tk.END, row)
+
+def clear_command(self):
+	self.list1.delete(0, tk.END)
