cd5b86897569281ef9db16ccab5fd2e0b970bfcf
Banksy <zukiewicz.piotr@gmail.com>
2020-10-10 10:26:26 +0200

    Add functionality to the buttons
    Imported the class methods from the main_screen_methods.py to add
    functionality to all the buttons of the main screen.

--- Library/frontend/main_screen.py
+++ Library/frontend/main_screen.py
@@ -1,15 +1,20 @@
 import sys
+
 import tkinter as tk
 
 sys.path.insert(0, '../backend')
-import backend_methods as lb
+from backend_methods import DatabaseConnection
+
 
 class MainScreen:
-	from frontend_commands import view_command
+
+	from main_screen_methods import (get_selected_row,
+	view_command, search_command, insert_command,
+	delete_command, update_command, clear_command)
 
 	def __init__(self, master):
 		#establish connection with the database
-		self.db_conn = lb.DatabaseConnection("library_backend.db")
+		self.db_conn = DatabaseConnection("library_database.db")
 
 		#root of the app
 		self.master = master
@@ -28,34 +33,35 @@ class MainScreen:
 		l4.grid(row=1, column=2)
 
 		#placeholders for the user inputs
-		title_text = tk.StringVar()
-		author_text = tk.StringVar()
-		year_text = tk.StringVar()
-		year_read_text = tk.StringVar()
+		self.title_text = tk.StringVar()
+		self.author_text = tk.StringVar()
+		self.year_text = tk.StringVar()
+		self.year_read_text = tk.StringVar()
 
 		#add entry boxes
-		e1 = tk.Entry(self.master, textvariable=title_text)
-		e2 = tk.Entry(self.master, textvariable=author_text)
-		e3 = tk.Entry(self.master, textvariable=year_text)
-		e4 = tk.Entry(self.master, textvariable=year_read_text)
+		self.e1 = tk.Entry(self.master, textvariable=self.title_text)
+		self.e2 = tk.Entry(self.master, textvariable=self.author_text)
+		self.e3 = tk.Entry(self.master, textvariable=self.year_text)
+		self.e4 = tk.Entry(self.master, textvariable=self.year_read_text)
 
 		#position the entry boxes
-		e1.grid(row=0, column=1)
-		e2.grid(row=1, column=1)
-		e3.grid(row=0, column=3)
-		e4.grid(row=1, column=3)
+		self.e1.grid(row=0, column=1)
+		self.e2.grid(row=0, column=3)
+		self.e3.grid(row=1, column=1)
+		self.e4.grid(row=1, column=3)
 
 		#add and position the listbox
 		self.list1 = tk.Listbox(self.master, height=6, width=35)
 		self.list1.place(x=20, y=60, width=220, height=120)
+		self.list1.bind('<<ListboxSelect>>',self.get_selected_row)
 
 		#add buttons
 		b1 = tk.Button(self.master, text="View all", width=10, command=self.view_command)
-		b2 = tk.Button(self.master, text="Search entry", width=10)
-		b3 = tk.Button(self.master, text="Add entry", width=10)
-		b4 = tk.Button(self.master, text="Update", width=10)
-		b5 = tk.Button(self.master, text="Delete", width=10)
-		b6 = tk.Button(self.master, text="Clear list", width=10)
+		b2 = tk.Button(self.master, text="Search entry", width=10, command=self.search_command)
+		b3 = tk.Button(self.master, text="Add entry", width=10, command=self.insert_command)
+		b4 = tk.Button(self.master, text="Update", width=10, command=self.update_command)
+		b5 = tk.Button(self.master, text="Delete", width=10, command=self.delete_command)
+		b6 = tk.Button(self.master, text="Clear list", width=10, command=self.clear_command)
 
 		#position the buttons
 		b1.grid(row=2, column=3)
